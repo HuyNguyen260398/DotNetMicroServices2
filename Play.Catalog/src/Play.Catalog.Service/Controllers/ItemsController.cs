46fa3ca422251e038fecbb01b9dffd46ff83b7cc
Huy Nguyen <huynguyen260398@gmail.com>
2022-03-20 09:34:09 +0700

    Added Update endpoint.

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -24,9 +24,29 @@ namespace Play.Catalog.Service.Controllers
         [HttpPost]
         public ActionResult<ItemDto> Create(CreateItemDto dto)
         {
-            var item = new ItemDto(Guid.NewGuid(), dto.Name, dto.Description, dto.Price, DateTimeOffset.UtcNow);
-            _items.Add(item);
-            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+            var newItem = new ItemDto(Guid.NewGuid(), dto.Name, dto.Description, dto.Price, DateTimeOffset.UtcNow);
+            _items.Add(newItem);
+            return CreatedAtAction(nameof(GetById), new { id = newItem.Id }, newItem);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, UpdateItemDto dto)
+        {
+            var existItem = _items.Find(i => i.Id == id);
+            if (existItem == null)
+                return NotFound();
+
+            var updatedItem = existItem with
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price
+            };
+
+            var index = _items.FindIndex(existItem => existItem.Id == id);
+            _items[index] = updatedItem;
+
+            return NoContent();
         }
     }
 }
